a79a035959a555e1a5e70a33b17b30822667b988
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2022-06-06 22:42:20 -0300

    feat: carga de nivel, agregar inputs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">
+      <div v-for="(nivel, i) in form" class="row nivel-padre" :key="i">
+        <div class="row row-cols-2 nivel">
+          <div class="col-10">
+            <div class="row row-cols-3">
+              <div v-for="(input, k) in nivel" class="col" :key="k">
+                <label v-bind:for="input.id" class="form-label">{{ input.label }}</label>
+                <input v-bind:type="input.type" class="form-control" v-bind:id="input.id"
+                  v-bind:placeholder="input.placeholder">
+              </div>
+            </div>
+          </div>
+          <cargarInputVue :indice="i" v-on:cargarInput="cargarInput"></cargarInputVue>
+        </div>
+        <div class="row justify-content-center">
+          <button class="add" v-on:click="addnivel(i)">
+            <span class="material-symbols-rounded">
+              add
+            </span>
+          </button>
+          <button class="add" v-on:click="removenivel(i)">
+            <span class="material-symbols-rounded">
+              delete
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import cargarInputVue from './components/cargarInput.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    cargarInputVue
+  },
+  data() {
+    return {
+      form: [
+        [
+          {
+            name: 'nombre',
+            id: 'nombre',
+            label: 'Nombre:',
+            type: 'text',
+            placeholder: ''
+          },
+          {
+            name: 'apellido',
+            id: 'apllido',
+            label: 'Apellido:',
+            type: 'text',
+            placeholder: ''
+          }
+        ],
+      ]
+    }
+  },
+  methods: {
+    cargarInput(data) {
+      this.form[data.id].push(data.data);
+    },
+    addnivel(i) {
+      console.log(i);
+      if (this.form.length <= 1 || i == this.form.length - 1) {
+        this.form.push([]);
+      } else {
+        if (i < this.form.length) {
+          var inicio = this.form.slice(0, i + 1);
+          var final = this.form.slice(i + 1);
+          console.log(inicio, final);
+          inicio.push([]);
+          this.form = inicio.concat(final);
+        }
+      }
+    },
+    removenivel(i) {
+      console.log(i);
+      if (this.form.length <= 1) {
+        this.form = [[]];
+      } else if( i == this.form.length - 1){
+        var antepenultimo = this.form.slice(0,i);
+        this.form = antepenultimo;
+      } {
+        if (i < this.form.length - 1) {
+          var inicio = this.form.slice(0, i);
+          var final = this.form.slice(i+1);
+          console.log(inicio, final);
+          this.form = inicio.concat(final);
+        }
+      }
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.nivel {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.nivel-padre {
+  display: flex;
+  flex-direction: column;
+
+}
+
+.add {
+  width: 40px;
+  height: 40px;
+  border: none;
+  background-color: white;
+  border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.add:hover {
+  background-color: darkgrey;
 }
 </style>
---
+++ src/components/cargarInput.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="col-2">
+        <button type="button" class="add" data-bs-toggle="modal" :data-bs-target="button">
+            <span class="material-symbols-rounded">
+                add
+            </span>
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" v-bind:id="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            Cargar Input
+                        </h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <label for="Label">Nombre</label>
+                            <input type="text" v-model="nombre" class="form-control">
+                        </div>
+                        <div class="row">
+                            <label for="">Id</label>
+                            <input type="text" v-model="id" class="form-control">
+                        </div>
+                        <div class="row">
+                            <label for="">Label</label>
+                            <input type="text" v-model="label" class="form-control">
+                        </div>
+                        <div class="row">
+                            <label for="">Type</label>
+                            <select name="" class="form-select" v-model="type">
+                                <option value="text">Text</option>
+                                <option value="number">Number</option>
+                                <option value="password">Password</option>
+                            </select>
+                        </div>
+                        <div class="row">
+                            <label for="">Placeholder</label>
+                            <input type="text" v-model="placeholder" class="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" v-on:click="cargarInput">Cargar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: "cargaInput",
+    props: {
+        indice: Number
+    },
+    mounted() {
+        this.modal = `modal${this.indice}`;
+        this.button = `#modal${this.indice}`;
+    },
+    data() {
+        return {
+            modal: "modal",
+            button: "#modal",
+            name: '',
+            id: '',
+            label: '',
+            type: '',
+            placeholder: ''
+        }
+    },
+    methods: {
+        cargarInput() {
+            this.$emit('cargarInput', {
+                id: this.indice,
+                data: {
+                    name: this.name,
+                    id: this.id,
+                    label: this.label,
+                    type: this.type,
+                    placeholder: this.placeholder
+                }
+            });
+        }
+    }
+}
+</script>
+<style>
+
+</style>
